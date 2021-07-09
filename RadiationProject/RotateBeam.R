baa43acb6cbf8279e3a3cc8cd2031aa30dbff594
AnikaWatson <abwatson@questu.ca>
2021-07-08 19:29:22 -0700

    Updated to include final R outputs

--- RadiationProject/RotateBeam.R
+++ RadiationProject/RotateBeam.R
@@ -29,6 +29,12 @@ dose <- read.csv(paste(inputlocation, data.files[1], sep=""), stringsAsFactors =
 str(dose)
 #looks good
 
+for(i in 1:iterationsx){
+  for(j in 1:iterationsy){
+    dose[i,j] <- (dose[i,j])/1000
+  }
+}
+
 ######## Rotating the Beam #####
 #We are going to populate a new grid of numbers with the strength of the angled beam at each point
 #First let's tell the computer how many iterations we need to fill the grid
@@ -46,20 +52,27 @@ for(i in 1:iterationsx){
   for(j in 1:iterationsy){
     vec = matrix(c((i-25),(j-25)), nrow = 2, ncol = 1)
     rot = A1 %*% vec
-    if(norm(rot)<24){
-    output1[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
-    } else {
-      output1[i,j] <- 0}
+    if(round(rot[1])<=-25){
+      output1[i,j] <- 0
+    } else {if(round(rot[1])>=25){
+      output1[i,j] <- 0
+    } else {if(round(rot[2])>=25){
+      output1[i,j] <- 0
+    } else {if(round(rot[2])<=-25){
+      output1[i,j] <- 0
+    } else {output1[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
+    }}
     }
+    }
+  }
 }
+
 #Let's check to make sure that the matrix was populated properly
 output1
 #And let's take a peek to ensure we rotated the beam and didn't cause complete chaos
+pdf(paste(wd, "/figures/Beam Rotated by theta_1.pdf", sep=""))
 imagesc(output1, xlab="", ylab="", col=jet.colors(16))
-#looks good, now let's turn that grid which is technically a matrix into a data frame
-output1 <- data.frame(output1)
-#and double check that the last step worked
-class(output1)
+dev.off()
 
 #Now that we have the code for one rotated beam we can repeat this process for however many beams we have
 #set the angle
@@ -74,23 +87,30 @@ for(i in 1:iterationsx){
   for(j in 1:iterationsy){
     vec = matrix(c((i-25),(j-25)), nrow = 2, ncol = 1)
     rot = A2 %*% vec
-    if(norm(rot)<24){
-      output2[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
-    } else {
-      output2[i,j] <- 0}
+    if(round(rot[1])<=-25){
+      output2[i,j] <- 0
+    } else {if(round(rot[1])>=25){
+      output2[i,j] <- 0
+    } else {if(round(rot[2])>=25){
+      output2[i,j] <- 0
+    } else {if(round(rot[2])<=-25){
+      output2[i,j] <- 0
+    } else {output2[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
+    }}
+    }
+    }
   }
 }
 #make sure that the matrix was populated properly
 output2
 #ensure we rotated the beam and didn't cause complete chaos
+pdf(paste(wd, "/figures/Beam Rotated by theta_2.pdf", sep=""))
 imagesc(output2, xlab="", ylab="", col=jet.colors(16))
-#turn that grid which is technically a matrix into a data frame
-output2 <- data.frame(output2)
-#and double check that the last step worked
-class(output2)
+dev.off()
+
 
 #set the angle
-theta3 = pi/4
+theta3 = pi/58
 #define the rotation matrix
 rotation3 = c(cos(theta3), sin(theta3), -sin(theta3), cos(theta3))
 A3 = matrix(rotation3, nrow = 2, ncol = 2)
@@ -101,20 +121,27 @@ for(i in 1:iterationsx){
   for(j in 1:iterationsy){
     vec = matrix(c((i-25),(j-25)), nrow = 2, ncol = 1)
     rot = A3 %*% vec
-    if(norm(rot)<24){
-      output3[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
-    } else {
-      output3[i,j] <- 0}
+    if(round(rot[1])<=-25){
+      output3[i,j] <- 0
+    } else {if(round(rot[1])>=25){
+      output3[i,j] <- 0
+      } else {if(round(rot[2])>=25){
+        output3[i,j] <- 0
+      } else {if(round(rot[2])<=-25){
+        output3[i,j] <- 0
+      } else {output3[i,j] <- dose[(round(rot[1])+25), (round(rot[2])+25)]
+      }}
+      }
+    }
   }
 }
-#make sure that the matrix was populated
+#make sure that the matrix was populated   
 output3
 #ensure we rotated the beam and didn't cause complete chaos
+pdf(paste(wd, "/figures/Beam Rotated by theta_3.pdf", sep=""))
 imagesc(output3, xlab="", ylab="", col=jet.colors(16))
-#turn that grid into a data frame
-output3 <- data.frame(output3)
-#and double check that the last step worked
-class(output3)
+dev.off()
+
 
 ####### Finding the Total Dosage at Each Point #######
 #Now we know how much dose each point gets from each beam
@@ -131,11 +158,10 @@ for(i in 1:iterationsx){
 #make sure that the matrix was populated
 TotalDose
 #ensure we rotated the beam and didn't cause complete chaos
+pdf(paste(wd, "/figures/Total Dose.pdf", sep=""))
 imagesc(TotalDose, xlab="", ylab="", col=jet.colors(16))
-#turn that grid into a data frame
-TotalDose <- data.frame(TotalDose)
-#and double check that the last step worked
-class(TotalDose)
+dev.off()
+
 
 
 ##### Isolating the Tumour #####
@@ -144,7 +170,9 @@ class(TotalDose)
 #To do so we need a tumour, so let's load some tumour data
 Tumour1 <- read.csv(paste(inputlocation, data.files[3], sep=""), stringsAsFactors = FALSE )
 #Let's see what the data looks like
-str(Tumour1)
+pdf(paste(wd, "/figures/Tumour1.pdf", sep=""))
+imagesc(as.matrix(Tumour1), xlab="", ylab="", col=jet.colors(16))
+dev.off()
 #looks good
 
 #Let's make a data frame of the total dose received at each point in the tumour
@@ -160,34 +188,34 @@ for(i in 1:iterationsx){
 #make sure that the matrix was populated
 TumourDose1
 #take a peek
+pdf(paste(wd, "/figures/Tumour1 Dose.pdf", sep=""))
 imagesc(TumourDose1, xlab="", ylab="", col=jet.colors(16))
-#turn the grid into a data frame
-TumourDose1 <- data.frame(TumourDose1)
-#and double check that the last step worked
-class(TumourDose1)
+dev.off()
+
 
 ##### Calculating the Survival Rate at Each Point in the Tumour ####
 #Now we know the dose received at each point in the tumour, but we want to know if it lives
 #First we set alpha and beta as defined in the paper
-a=1
-b=1
-
+a=0.01
+b=0.0025
+exp(-(a*(TumourDose1[20,30])+b*(TumourDose1[20,30])^2))
+options(digits = 22)
 #Next we need a data frame of the cell survival
 CellSurvival1 <- matrix(ncol=iterationsy, nrow=iterationsx)
 
 for(i in 1:iterationsx){
   for(j in 1:iterationsy){
-    CellSurvival1[i,j] <- a*(TumourDose1[i,j])+b*(TumourDose1[i,j])^2
+    CellSurvival1[i,j] <- 100*exp(-(a*(TumourDose1[i,j])+b*(TumourDose1[i,j])^2))
   }
 }
 #make sure that the matrix was populated
 CellSurvival1
 #Take a peek
+pdf(paste(wd, "/figures/Tumor1 S(p).pdf", sep=""))
 imagesc(CellSurvival1, xlab="", ylab="", col=jet.colors(16))
+dev.off()
 #turn the grid into a data frame
-CellSurvival1 <- data.frame(CellSurvival1)
-#and double check that the last step worked
-class(CellSurvival1)
+
 
 #Now let's make a histogram to visualize how zapped the tumour was
 #First we need to get rid of all the zeros everywhere outside the tumour
@@ -201,10 +229,12 @@ for(i in 1:iterationsx){
       HowZapped1[i,j] <- NA}
   }
 }
-HowZapped1 <- data.frame(HowZapped)
+HowZapped1 <- data.frame(HowZapped1)
 
 #now we can safely create a histogram
-hist(unlist(HowZapped1))
+pdf(paste(wd, "/figures/Tumor1 S(p) Histogram.pdf", sep=""))
+hist(unlist(HowZapped1), xlab = "S(p)", col = "red")
+dev.off()
 
 ##### Quantifying How Much of the Tumour Survives #####
 
@@ -248,6 +278,9 @@ Tumour2 <- read.csv(paste(inputlocation, data.files[4], sep=""), stringsAsFactor
 #Let's see what the data looks like
 str(Tumour2)
 #looks good
+pdf(paste(wd, "/figures/Tumor2.pdf", sep=""))
+imagesc(as.matrix(Tumour2), xlab="", ylab="", col=jet.colors(16))
+dev.off()
 
 #Let's make a data frame of the total dose received at each point in the tumour
 TumourDose2 <- matrix(ncol=iterationsy, nrow=iterationsx)
@@ -261,11 +294,9 @@ for(i in 1:iterationsx){
 #make sure that the matrix was populated
 TumourDose2
 #check it out
+pdf(paste(wd, "/figures/Tumor2 Dose.pdf", sep=""))
 imagesc(TumourDose2, xlab="", ylab="", col=jet.colors(16))
-#turn that grid into a data frame
-TumourDose2 <- data.frame(TumourDose2)
-#and double check that the last step worked
-class(TumourDose2)
+dev.off()
 
 #Let's make a data frame of the cell survival
 CellSurvival2 <- matrix(ncol=iterationsy, nrow=iterationsx)
@@ -273,17 +304,15 @@ CellSurvival2 <- matrix(ncol=iterationsy, nrow=iterationsx)
 #And now we can populate that grid
 for(i in 1:iterationsx){
   for(j in 1:iterationsy){
-    CellSurvival2[i,j] <- a*(TumourDose2[i,j])+b*(TumourDose2[i,j])^2
+    CellSurvival2[i,j] <- 100*exp(-(a*(TumourDose2[i,j])+b*(TumourDose2[i,j])^2))
   }
 }
 #make sure that the matrix was populated
 CellSurvival2
 #visualize
+pdf(paste(wd, "/figures/Tumor2 S(p).pdf", sep=""))
 imagesc(CellSurvival2, xlab="", ylab="", col=jet.colors(16))
-#turn that grid into a data frame
-CellSurvival2 <- data.frame(CellSurvival2)
-#and double check that the last step worked
-class(CellSurvival2)
+dev.off()
 
 #Now let's make a histogram' to visualize how zapped the tumour was
 #Get rid of unecessary the zeros
@@ -297,9 +326,9 @@ for(i in 1:iterationsx){
   }
 }
 HowZapped2 <- data.frame(HowZapped2)
-
-hist(unlist(HowZapped2))
-
+pdf(paste(wd, "/figures/Tumor2 S(p) Histogram.pdf", sep=""))
+hist(unlist(HowZapped2), xlab = "S(p)", col = "red")
+dev.off()
 
 # Now let's see how many tumour cells died
 deadcells2 = 0
